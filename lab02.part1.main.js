873f51ab7cf3da2b663f7ba023be352be646d553
bxfox2 <barry.fox@centurylink.com>
2020-12-29 23:04:09 +0000

    Lab 2 part 1 of 2

--- lab02.part1.main.js
+++ lab02.part1.main.js
@@ -5,19 +5,20 @@
   Assign the class definition to variable IPCIDR.
 */
 const IPCIDR = require('ip-cidr');
+
 /**
  * Calculate and return the first host IP address from a CIDR subnet.
  * @param {string} cidrStr - The IPv4 subnet expressed
  *                 in CIDR format.
  * @param {callback} callback - A callback function.
- * @return {object} An object containing both an IPv4 address and
-  *                    its mapped ipv6 address.
+ * @return {string} (firstIpAddress) - An IPv4 address.
  */
 function getFirstIpAddress(cidrStr, callback) {
+
   // Initialize return arguments for callback
   let firstIpAddress = null;
   let callbackError = null;
-  let secondIPV6 = null;
+
   // Instantiate an object from the imported class and assign the instance to variable cidr.
   const cidr = new IPCIDR(cidrStr);
   // Initialize options for the toArray() method.
@@ -36,19 +37,14 @@ function getFirstIpAddress(cidrStr, callback) {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
-    secondIPV6 = getIpv4MappedIpv6Address(firstIpAddress)
   }
-  const result = {
-    ipv4: firstIpAddress,
-    ipv6: secondIPV6
-  };
-
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-  return callback(result, callbackError);
+  return callback(firstIpAddress, callbackError);
 }
+
 /**
  * Calculates an IPv4-mapped IPv6 address.
  * @param {string} ipv4 - An IPv4 address in dotted-quad format.
@@ -66,7 +62,7 @@ function getIpv4MappedIpv6Address(ipv4) {
   let numIpv4Segments = ipv4Quads.length;
 
   // Verify IPv4 had four parts.
-if (numIpv4Segments === 4) {
+  if (numIpv4Segments === 4) {
     let validQuads = true;
     // Iterate over the IPv4 address parts and verify each segment was a number between 0 and 255.
     for(let i=0; i < numIpv4Segments; i++) {
@@ -98,6 +94,7 @@ if (numIpv4Segments === 4) {
   return ipv6Address;
 }
 
+
 /*
   This section is used to test function and log any errors.
   We will make several positive and negative tests.
@@ -121,7 +118,7 @@ function main() {
       if (error) {
         console.error(`  Error returned from GET request: ${error}`);
       }
-      console.log(`  Response returned from GET request: \{\"ipv4\":\"${data.ipv4}\",\"ipv6\":\"${data.ipv6}\"}`);
+      console.log(`  Response returned from GET request: ${data}`);
     });
   }
   // Iterate over sampleIpv4s and pass the element's value to getIpv4MappedIpv6Address().
