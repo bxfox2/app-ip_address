73c2bc02562936cc3a1b3ca38c137480f8b37573
bxfox2 <barry.fox@centurylink.com>
2020-12-29 01:09:57 +0000

    Lab 4

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ class IpAddress {
 
     // Calling the getIpv4MappedIpv6Address() to calculate the IPv4-mapped IPv6 address for the passed IPv4 address.
     firstIpAddress.ipv6 = getIpv4MappedIvp6Address(firstIpAddress.ipv4);
+    }
   
 
     // Call the passed callback function.
