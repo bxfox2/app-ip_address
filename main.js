6ff6ee67b6b0e72c46041e0193cd0205c229664d
bxfox2 <barry.fox@centurylink.com>
2020-12-29 19:55:49 +0000

    Lab 4

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ class IpAddress {
         // If the passed CIDR is valid, call the object's toArray() method.
         // Notice the destructering assignment syntax to get the value of the first array's element.
         [firstIpAddress] = cidr.toArray(options);
-         firstIpAddress.ipv6 = getIpv4MappedIvp6Address(firstIpAddress.ipv4);
+         firstIpAddress = getIpv4MappedIvp6Address(firstIpAddress);
     }
 
       
